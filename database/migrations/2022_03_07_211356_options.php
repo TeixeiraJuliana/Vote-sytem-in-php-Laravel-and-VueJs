c79f64cf4e68dc875a76bcd0d1a264be35ce7d66
juliana teixeira <teixeirajulianakellys@gmail.com>
2022-03-11 11:41:28 -0300

    dabase ok

--- database/migrations/2022_03_07_211356_options.php
+++ database/migrations/2022_03_07_211356_options.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('option', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('theme_id')->unsigned();
-            $table->foreign('theme_id')->references('id')->on('theme');
+            $table->foreign('theme_id')->references('id')->on('themes');
             $table->string('option');
             $table->integer('how-many')->default(0);
             $table->softDeletes();
